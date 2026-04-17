30d21044238af6fe6718c803881150415dc4c638
Andrey (MiTask) <61359286+MrMasrozYTLIVE@users.noreply.github.com>
2026-04-17 12:15:55 +0300

    Update Utils.java
    Added link to the original file and also cleaned up the code

--- src/main/java/dev/mitask/vibrational/Utils.java
+++ src/main/java/dev/mitask/vibrational/Utils.java
@@ -1,9 +1,11 @@
 package dev.mitask.vibrational;
 
+// https://github.com/henkelmax/voicechat-interaction/blob/master/src/main/java/de/maxhenkel/vcinteraction/AudioUtils.java
 public class Utils {
     /**
      * Calculates the audio level of a signal with specific samples.
      *
+     * @author Henhelmax
      * @param samples the samples of the signal to calculate the audio level of
      * @return the audio level of the specified signal in db
      */
@@ -19,14 +21,8 @@ public class Utils {
 
         rms = (sampleCount == 0) ? 0 : Math.sqrt(rms / sampleCount);
 
-        double db;
-
-        if (rms > 0D) {
-            db = Math.min(Math.max(20D * Math.log10(rms), -127D), 0D);
-        } else {
-            db = -127D;
-        }
-
-        return db;
+        if(rms <= 0D) return -127D;
+        // dB
+        return Math.min(Math.max(20D * Math.log10(rms), -127D), 0D);
     }
 }
